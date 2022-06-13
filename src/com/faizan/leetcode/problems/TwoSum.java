e948f15cbb5fe9969655a465de19860b4c204260
Md Faizan <md.faizan3664@gmail.com>
2022-06-13 09:02:55 +0530

    optimize two sum using hashtable

--- src/com/faizan/leetcode/problems/TwoSum.java
+++ src/com/faizan/leetcode/problems/TwoSum.java
@@ -1,30 +1,18 @@
 package com.faizan.leetcode.problems;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
-    public  int[] twoSum(int[] nums, int target) {
-        int[] array = nums.clone();
-        Arrays.sort(array);
-        for(int i=0; i<array.length-1; i++) {
-            int j = Arrays.binarySearch(array, target - array[i]);
-            if(j>=0) return getIndex(nums, array[i], array[j]);
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            if(map.containsKey(target-nums[i])) {
+                return new int[] {map.get(target-nums[i]), i};
+            }
+            map.put(nums[i], i);
         }
         return new int[2];
-    }
-
-    private int[] getIndex(int[] nums, int first, int second) {
-        int[] result = new int[]{-1, -1};
-        for(int i = 0; i<nums.length; i++) {
-            if(result[0] != -1 && result[1] != -1) return result;
-            if(result[0] == -1 && nums[i] == first) {
-                result[0] = i;
-                continue;
-            }
-
-            if(result[1] == -1 && nums[i] == second) result[1] = i;
-        }
-        return result;
     }
 }
