21b2c3f7f0f4ea3a02f956202ff0ed2e464f7161
Md Faizan <md.faizan3664@gmail.com>
2022-06-13 09:08:27 +0530

    optimize two sum using hashtable

--- src/com/faizan/leetcode/problems/TwoSum.java
+++ src/com/faizan/leetcode/problems/TwoSum.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
         for(int i=0; i<nums.length; i++) {
             if(map.containsKey(target-nums[i])) {
                 return new int[] {map.get(target-nums[i]), i};
