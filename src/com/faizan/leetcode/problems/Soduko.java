3033523a52b849c0063f576f00faf2f33d41d199
mdfaizan3664 <md.faizan3664@gmail.com>
2022-07-01 23:03:55 +0530

    optimize solve soduko problem

--- src/com/faizan/leetcode/problems/Soduko.java
+++ src/com/faizan/leetcode/problems/Soduko.java
@@ -3,16 +3,7 @@ package com.faizan.leetcode.problems;
 import java.util.*;
 
 public class Soduko {
-    public static void main(String[] args) {
-        char[][] board = {{'.','.','9','7','4','8','.','.','.'},{'7','.','.','.','.','.','.','.','.'},{'.','2','.','1','.','9','.','.','.'},{'.','.','7','.','.','.','2','4','.'},{'.','6','4','.','1','.','5','9','.'},{'.','9','8','.','.','.','3','.','.'},{'.','.','.','8','.','3','.','2','.'},{'.','.','.','.','.','.','.','.','6'},{'.','.','.','2','7','5','9','.','.'}};
-        solveSudoku(board);
-    }
-    public static List<Character> characters = new ArrayList<>(10);
-    public static Hashtable<Integer, Set<Character>> row = new Hashtable<>();
-    public static Hashtable<Integer, Set<Character>> coloumn = new Hashtable<>();
-    public static Hashtable<Integer, Set<Character>> square = new Hashtable<>();
-    //sudoku solver
-    public static void solveSudoku(char[][] board) {
+    public Soduko() {
         characters.add(0, '0');
         characters.add(1, '1');
         characters.add(2, '2');
@@ -23,15 +14,20 @@ public class Soduko {
         characters.add(7, '7');
         characters.add(8, '8');
         characters.add(9, '9');
-        fill(board);
+    }
+
+    public static final List<Character> characters = new ArrayList<>(10);
+    //sudoku solver
+    public void solveSudoku(char[][] board) {
         solveSudoku(board, 0, 0);
     }
 
-    private static boolean solveSudoku(char[][] board, int i, int j) {
+    private boolean solveSudoku(char[][] board, int i, int j) {
         if(i == 9) return true;
         if (board[i][j] == '.') {
             for(int f = 1; f<=9; f++) {
-                if(fillBox(board, i, j, characters.get(f))){
+                if(isValidPlacement(board, i, j, characters.get(f))){
+                    board[i][j] = characters.get(f);
                     boolean b;
                     if(j>=8) {
                         b = solveSudoku(board, i + 1, 0);
@@ -39,9 +35,6 @@ public class Soduko {
                         b = solveSudoku(board, i, j+1);
                     }
                     if(!b) {
-                        remove(board[i][j], row, i);
-                        remove(board[i][j], coloumn,j);
-                        remove(board[i][j], square, (j/3)*3+(i/3));
                         board[i][j] = '.';
                     }
                     if(b) return true;
@@ -57,57 +50,42 @@ public class Soduko {
         }
     }
 
-    private static boolean fillBox(char[][] board, int i, int j, Character num) {
-        if(!fill(num, row, i)) return false;
-        if(!fill(num, coloumn,j)) {
-            remove(num, row, i);
-            return false;
-        }
-        if(!fill(num, square, (j/3)*3+(i/3))) {
-            remove(num, row, i);
-            remove(num, coloumn,j);
-            return false;
-        }
-        board[i][j] = num;
-        return true;
-    }
-
-    public static boolean fill(char[][] board) {
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board[0].length; j++) {
-                if(board[i][j] == '.') {
-                    continue;
-                }
-                if(!fill(board[i][j], row, i)) return false;
-                if(!fill(board[i][j], coloumn,j)) return false;
-                if(!fill(board[i][j], square, (j/3)*3+(i/3))) return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean fill(char value, Hashtable<Integer, Set<Character>> table, int index) {
-        Set<Character> integers = null;
-        if(table.containsKey(index)) {
-            integers = table.get(index);
-            if(integers.contains(value)) return false;
-        }else {
-            integers = new HashSet<>();
-        }
-        integers.add(value);
-        table.put(index, integers);
-        return true;
-    }
-    public static boolean remove(char value, Hashtable<Integer, Set<Character>> table, int index) {
-        if(table.containsKey(index)) {
-            Set<Character> integers = table.get(index);
-            if(integers.contains(value)) {
-                integers.remove(value);
-                table.put(index, integers);
+    private static boolean isNumberInRow(char[][] board, int number, int row) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == number) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static boolean isNumberInColumn(char[][] board, int number, int column) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInBox(char[][] board, int row, int column, Character number) {
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+
+        for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPlacement(char[][] board, int row, int column, Character number) {
+        return !isNumberInRow(board, number, row) &&
+                !isNumberInColumn(board, number, column) &&
+                !isNumberInBox(board, row, column, number);
     }
 
 }
